377887356302d7c5be576d2703a9f2eb5d84ada1
Rami Abdelrazzaq <ramiabdelrazzaq@gmail.com>
2026-08-11 13:15:08 -0500

    test: cover browser preview server cleanup

---
+++ tests/utils/test_show.py
@@ -0,0 +1,37 @@
+from altair.utils import _show
+
+
+def test_open_html_in_browser_closes_server(monkeypatch):
+    events = []
+
+    class Browser:
+        def open(self, url):
+            events.append(("open", url))
+
+    class FakeServer:
+        server_port = 4321
+
+        def __init__(self, address, handler):
+            events.append(("init", address, handler.__name__))
+
+        def __enter__(self):
+            events.append("enter")
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            events.append("exit")
+
+        def handle_request(self):
+            events.append("handle")
+
+    monkeypatch.setattr(_show.webbrowser, "get", lambda _: Browser())
+    monkeypatch.setattr(_show, "HTTPServer", FakeServer)
+
+    _show.open_html_in_browser("<p>hello</p>")
+
+    assert events[1:] == [
+        "enter",
+        ("open", "http://127.0.0.1:4321"),
+        "handle",
+        "exit",
+    ]
