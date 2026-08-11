6be4b3548e419edfc6535a7dafb2e7ba0f3346b1
Rami Abdelrazzaq <ramiabdelrazzaq@gmail.com>
2026-08-11 13:14:28 -0500

    fix: close browser preview HTTP server

--- altair/utils/_show.py
+++ altair/utils/_show.py
@@ -68,8 +68,8 @@ def open_html_in_browser(
             pass
 
     # Use specified port if provided, otherwise choose a random port (port value of 0)
-    server = HTTPServer(
+    with HTTPServer(
         ("127.0.0.1", port if port is not None else 0), OneShotRequestHandler
-    )
-    browser.open(f"http://127.0.0.1:{server.server_port}")
-    server.handle_request()
+    ) as server:
+        browser.open(f"http://127.0.0.1:{server.server_port}")
+        server.handle_request()
