e7c5ee4b63bd45a25386e3ab19eeb686dcb72e12
Rami Abdelrazzaq <ramiabdelrazzaq@gmail.com>
2026-08-12 14:05:47 -0500

    test: cover browser preview cleanup on error

--- tests/utils/test_show.py
+++ tests/utils/test_show.py
@@ -1,3 +1,5 @@
+import pytest
+
 from altair.utils import _show
 
 
@@ -34,4 +36,41 @@ def test_open_html_in_browser_closes_server(monkeypatch):
         ("open", "http://127.0.0.1:4321"),
         "handle",
         "exit",
+    ]
+
+
+def test_open_html_in_browser_closes_server_when_browser_open_fails(monkeypatch):
+    events = []
+
+    class Browser:
+        def open(self, url):
+            events.append(("open", url))
+            raise RuntimeError("browser failed")
+
+    class FakeServer:
+        server_port = 4321
+
+        def __init__(self, address, handler):
+            events.append(("init", address, handler.__name__))
+
+        def __enter__(self):
+            events.append("enter")
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            events.append(("exit", exc_type))
+
+        def handle_request(self):
+            events.append("handle")
+
+    monkeypatch.setattr(_show.webbrowser, "get", lambda _: Browser())
+    monkeypatch.setattr(_show, "HTTPServer", FakeServer)
+
+    with pytest.raises(RuntimeError, match="browser failed"):
+        _show.open_html_in_browser("<p>hello</p>")
+
+    assert events[1:] == [
+        "enter",
+        ("open", "http://127.0.0.1:4321"),
+        ("exit", RuntimeError),
     ]
